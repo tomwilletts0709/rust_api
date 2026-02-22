420fcd661d24d9f4275034c3a3714efdae58557b
Ashleigh Hewitt <ashleighhewitt@Ashleighs-MacBook-Pro-2.local>
2026-02-22 22:47:17 +0000

    altered main files

--- api/src/main.rs
+++ api/src/main.rs
@@ -9,16 +9,18 @@ mod models;
 mod schema;
 mod static_files;
 
-fn main() {
-    dotenv::from_path("../.env").ok();
-    dotenv().ok();
+fn rocket() -> rocket::Rocket{
+    dotenv().ok(); 
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = db::init_pool(&database_url);
-
-    rocket::build()
+    
+    let pool = db::init_pool(database_url);
+    rocket::ignite()
         .manage(pool)
-        .mount("/", routes![static_files::index, static_files::all])
-        .launch();
+        .mount("/", routes![static_files::all, static_files::index])
+
 }
 
+fn main() {
+    rocket().launch();
+}
