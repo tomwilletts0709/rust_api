f507b4b8853672de627402776d8113a39d366121
Ashleigh Hewitt <tom_94_oasis@hotmail.com>
2026-03-29 19:38:08 +0100

    changes to routes

--- api/src/routes.rs
+++ api/src/routes.rs
@@ -20,3 +20,25 @@ fn new(conn: Dbconn, new_book: Json<NewBook>) -> Json<Value> {
         "results": Book::all(&conn).first()
     }))
 }
+
+#[get("/books/<id>", format = "application/json")]
+fn show(conn: DbConn, id: i32) -> Json<Value> {
+    let result = Book::show(id, &conn);
+    let status - if result.is_empty() {404} else {200};
+
+    Json(json!({
+        "status": status,
+        "result": result.get(0),
+    }))
+}
+
+#[put("/books/<id>", format = "application/json", data = "<book>")]
+fn update(conn: DbConn, id: i32, book: Json<NewBook>) -> Json<Value> {
+    let status = Book::update_by_id(id, &conn, book.into_inner());
+    let result = Book::show(id, &conn);
+
+    Json(json!({
+        "status": status,
+        "result": result.get(0),
+    }))
+}
